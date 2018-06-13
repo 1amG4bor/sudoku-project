acca9de09d5c3627de683a8be26620a6bdef4675
g4bor <gabor.tanacs@yahoo.com>
2018-06-13 16:52:24 +0200

    reorganize_Rendering_Algorythm

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-// Sudoku v0.0.1
-// const RLS = require('readline-sync');
+// ###===---> Sudoku v0.0.1 <---===###
+
+// npm Packages:
+const RLS = require('readline-sync');
 const mapping = require('./mapping');
 let Array2d = require('array-2d');
 let { table } = require('table');
+
+// declaration
 let output;
 let result = [];
 
-// START!!!
-
-let setting = mapping.setBoard(81);
+// Make board
+let setting = mapping.setBoard(36);
 let board = new Array2d(setting.xSize, setting.ySize, 0);
 board = mapping.generateBoard(setting);
 console.log(board.toString());
-
+// Draw the board
 for (let i = 0; i < setting.ySize; i++) {
   result[i] = [];
   for (let j = 0; j < setting.xSize; j++) {
     result[i][j] = board.get(i, j);
   }
 }
-
 output = table(result);
 console.log(output);
+
+// start test!
+let pressedKey;
+while (pressedKey !== 'esc') {
+  pressedKey = RLS.keyIn(['press any key: ']);
+  console.log(pressedKey + ' has pressed!');
+}
+
+// console.log('\x1Bc');
--- mapping.js
+++ mapping.js
@@ -27,6 +27,61 @@ const mapping = {
     return boardProperies;
   },
 
+  // Generate the Sudoku table based on the setting
+  generateBoard: (setting) => {
+    let board = new Array2d(setting.xSize, setting.ySize, 0);
+    let backtrackX = false;
+    let backtrackY = false;
+    let outerLaps;
+    let temp;
+    for (let x = 0; x < setting.xSize; x++) {
+      backtrackY = false;
+      outerLaps = 0;
+      for (let y = 0; y < setting.ySize; y++) {
+        backtrackX = false;
+        let laps = 0;
+        temp = mapping.validate(setting, board, x, y, laps, outerLaps, backtrackX, backtrackY);
+        // reOrganize code!!!
+        if (!backtrackX) {
+          board.set(x, y, temp);
+        } else mapping.backtracking(setting, board, x, y, outerLaps, backtrackY);
+      }
+    }
+    return board;
+  },
+
+  validate: (setting, board, x, y, laps, outerLaps, backtrackX, backtrackY) => {
+    let valid = false;
+    let temp;
+    while (!valid) {
+      laps++;
+      temp = mapping.getRandomInt(setting.xSize);
+      if (y === 0 && x === 0) valid = true;
+      else if (mapping.checkBoard(x, y, temp, setting, board)) valid = true;
+      if (laps > setting.xSize * setting.ySize) {
+        backtrackX = true;
+        if (outerLaps > 10) {
+          backtrackY = true;
+        }
+        break;
+      }
+    }
+    return temp;
+  },
+
+  // Do backtracking if dead-end happens
+  backtracking: (setting, board, x, y, outerLaps, backtrackY) => {
+    for (let i = 0; i < setting.xSize; i++) board.set(x, i, 0);
+    y = -1;
+    outerLaps++;
+    if (backtrackY) {
+      for (let j = x; j > x - 2; j--) {
+        for (let i = 0; i < setting.xSize; i++) board.set(j, i, 0);
+      }
+      x -= 2;
+    }
+  },
+
   // Generate random number based on the board size
   getRandomInt: (max) => {
     return Math.floor(Math.random() * Math.floor(max)) + 1;
@@ -51,54 +106,6 @@ const mapping = {
       }
     }
     return true;
-  },
-
-  // Generate the Sudoku table based on the setting
-  generateBoard: (setting) => {
-    let xSize = setting.xSize;
-    let ySize = setting.ySize;
-    let board = new Array2d(setting.xSize, setting.ySize, 0);
-    let valid;
-    let temp;
-    let backtrackX = false;
-    let backtrackY = false;
-    let outerLaps;
-    for (let x = 0; x < xSize; x++) {
-      backtrackY = false;
-      outerLaps = 0;
-      for (let y = 0; y < ySize; y++) {
-        valid = false;
-        backtrackX = false;
-        let laps = 0;
-        while (!valid) {
-          laps++;
-          temp = mapping.getRandomInt(xSize);
-          if (y === 0 && x === 0) valid = true;
-          else if (mapping.checkBoard(x, y, temp, setting, board)) valid = true;
-          if (laps > xSize * ySize) {
-            backtrackX = true;
-            if (outerLaps > 10) {
-              backtrackY = true;
-            }
-            break;
-          }
-        }
-        if (!backtrackX) {
-          board.set(x, y, temp);
-        } else {
-          for (let i = 0; i < xSize; i++) board.set(x, i, 0);
-          y = -1;
-          outerLaps++;
-          if (backtrackY) {
-            for (let j = x; j > x - 2; j--) {
-              for (let i = 0; i < xSize; i++) board.set(j, i, 0);
-            }
-            x -= 2;
-          }
-        }
-      }
-    }
-    return board;
   }
 
 };
